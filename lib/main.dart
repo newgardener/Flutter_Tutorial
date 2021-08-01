f76b9f90bcd68540c15fa8b7c0534dce86ab973a
JeongwonShin <jennyshin106@gmail.com>
2021-08-01 21:55:51 +0700

    feat: create product_manager & products

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.lightBlue,
+        accentColor: Colors.grey,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(startingProduct: 'Food Tester'),
+      ),
+    );
+  }
+}
---
+++ lib/product_manager.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'products.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+  ProductManager({this.startingProduct = 'Sweets Tester'}) {
+    print('[ProductManager Widget] Constructor');
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState');
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print('[ProductManager State] initState()');
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _products.add('Advanced Food Tester');
+              });
+            },
+            child: Text('Add Product')),
+      ),
+      Products(_products)
+    ]);
+  }
+}
